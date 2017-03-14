432ebbd2eccae82e0a9045076f2c1bb9a88dfa36
Mate Hegyhati <hegyhati.work@gmail.com>
2017-03-14 12:15:46 +0100

    Arguments

--- 0314/main.cpp
+++ 0314/main.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 using namespace std;
 
+#include <cstdlib>
+
 #include "gun.hpp"
 #include "zombie.hpp"
 #include "survivor.hpp"
 
 void shootZombie(Zombie& z, Survivor& s) {
 	try {
+		cout<<"\n\nShooting "<<z.getName()<<":\n";
 		while (z.getHP()>0){
 			s.pew(z);
-			cout<<"pew, HP decreased to "<<z.getHP()<<"\n";
+			cout<<"\tpew, HP decreased to "<<z.getHP()<<"\n";
 		}
 		cout<<"Yeppeeeee "<<z.getName()<<" diedied (again)\n";
 	} catch (OutOfAmmoException e) {
@@ -19,15 +22,22 @@ void shootZombie(Zombie& z, Survivor& s) {
 
 int main(int argc, char** argv){
 	
+	int zcount = (argc-1) / 3;
+	Zombie** horde = new Zombie*[zcount];
+	
+	for (int z =0;z<zcount; z++){
+		horde[z]=new Zombie(argv[3*z+1],atof(argv[3*z+2]),atof(argv[3*z+3]));
+	}
+	
+	
+	
 	Gun AK(30,1.2,0.2,5);
-	Zombie Stan("Stan",10,10);
-	Zombie Amanda("Amanda",10,33);
-	Survivor Deryl;
-	
-	
+	Survivor Deryl;	
 	Deryl.pickup(AK);
-	shootZombie(Stan,Deryl);
-	shootZombie(Amanda,Deryl);
+	
+	for(int z=0; z<zcount; z++)
+		shootZombie(*horde[z],Deryl);
+	
 	
 	
 	return 0;	
